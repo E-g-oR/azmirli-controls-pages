5720a2fc44f133c65b691dc314824182f58ba66e
Egor Muzychkin <emuzych@gmail.com>
2022-08-14 17:32:06 +0300

    Updates

--- assets/index.466cf4cd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.115109fe.js";import{T as r}from"./Typography.45f44efc.js";const n=()=>o(r,{variant:"h2",children:"Nothing found here"});export{n as default};
---
+++ assets/index.f3a282a0.js
@@ -0,0 +1,1 @@
+import{l as o}from"./index.cbf16599.js";import{T as r}from"./Typography.8d45c657.js";const n=()=>o(r,{variant:"h2",children:"Nothing found here"});export{n as default};
