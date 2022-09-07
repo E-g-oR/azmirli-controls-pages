33aee259203b53bfa890fdb0c755578c4e82c1ce
Egor Muzychkin <emuzych@gmail.com>
2022-09-07 23:45:40 +0300

    Updates

--- assets/index.05606577.js
+++
@@ -1,1 +0,0 @@
-import{j as u}from"./index.605d8d15.js";import{T as o}from"./Typography.770c5cb5.js";const a=()=>u(o,{level:"h3",children:"\u041D\u0438\u0447\u0435\u0433\u043E \u043D\u0435 \u043D\u0430\u0439\u0434\u0435\u043D\u043E ://"});export{a as default};
